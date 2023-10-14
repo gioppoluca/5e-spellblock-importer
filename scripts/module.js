ac4cc5e6a5d9a76e73cac2e449c0d3f701b4a538
Luca Gioppo <luca.gioppo@gmail.com>
2023-10-14 22:23:45 +0200

    fix: wrong localization of button

--- scripts/module.js
+++ scripts/module.js
@@ -18,7 +18,7 @@ Hooks.on("renderItemDirectory", (app, html, data) => {
     spbiUtils.log("Rendering spbi button");
 
     // Add the import button to the UI in the characters tab.
-    const importButton = $("<button id='sbi-main-button'><i class='fas fa-file-import'></i></i>{{ localize '5e-spellblock-importer.importTextTab' }}</button>");
+    const importButton = $("<button id='sbi-main-button'><i class='fas fa-file-import'></i></i>"+game.i18n.localize('5e-spellblock-importer.importTextTab')+"</button>");
     html.find(".directory-footer").append(importButton);
 
     importButton.click(async (ev) => {
